b7f9e63583f0bd6fd233f9d694094011944cf56b
Daniel <draycruz06@gmail.com>
2024-01-30 14:35:26 -0700

    reworked title

--- Project-C/Assets/Scripts/titleManager.cs
+++ Project-C/Assets/Scripts/titleManager.cs
@@ -4,15 +4,50 @@ using UnityEngine;
 
 public class titleManager : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public GameObject TitleScreen;
+    public GameObject HomeDashboard;
+    public GameObject Duels;
+    public GameObject Options;
+    public GameObject Practice;
+
+    private GameObject _currentScreen;
+    private List<GameObject> _screenCollection;
+
     void Start()
     {
-        
+        _screenCollection = new List<GameObject> { TitleScreen, HomeDashboard, Duels, Options, Practice };
+        _currentScreen = TitleScreen;
+
+        foreach (var screen in _screenCollection)
+        {
+            if (screen != null)
+            {
+                screen.SetActive(false);
+            }
+        }
+
+        if (_currentScreen != null)
+        {
+            _currentScreen.SetActive(true);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    public void ScreenTransition(GameObject targetScreen)
     {
-        
+        if (_currentScreen != null)
+        {
+            _currentScreen.SetActive(false);
+        }
+
+        if (targetScreen != null)
+        {
+            targetScreen.SetActive(true);
+            _currentScreen = targetScreen;
+        }
+    }
+
+    public void CoolFunction()
+    {
+        Debug.Log("CLICK!");
     }
 }
