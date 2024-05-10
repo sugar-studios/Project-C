341621c995b7d10efa4205ecde90ea3b4730b7eb
sugar studios <abcalkins8723@gmail.com>
2024-05-10 14:36:50 -0700

    added move data class

---
+++ Project-C/Assets/Scripts/Attacking/MoveData.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class MoveData
+{
+    public string name;
+    public Vector3 coordinates;
+    public string hitboxType;
+    public Vector3 rotation;
+    public float damage;
+    public float startup;
+    public float active;
+    public float endLag;
+
+    public MoveData(string name, Vector3 coordinates, string hitboxType, Vector3 rotation, float damage, float startup, float active, float endLag)
+    {
+        this.name = name;
+        this.coordinates = coordinates;
+        this.hitboxType = hitboxType;
+        this.rotation = rotation;
+        this.damage = damage;
+        this.startup = startup;
+        this.active = active;
+        this.endLag = endLag;
+    }
+}
+
+public class MoveSet
+{
+    public Dictionary<string, MoveData> moves = new Dictionary<string, MoveData>();
+
+    public MoveSet()
+    {
+        InitializeMoves();
+    }
+
+    private void InitializeMoves()
+    {
+        // Example initialization for one move
+        moves.Add("GroundedLeftRightNormal", new MoveData("GroundedLeftRightNormal", new Vector3(1, 0, 0), "Square", Vector3.zero, 10.0f, 0.1f, 0.5f, 0.2f));
+        // Add other moves similarly
+    }
+}
--- Project-C/Assets/Scripts/Player/PlayerAttacker.cs
+++ Project-C/Assets/Scripts/Player/PlayerAttacker.cs
@@ -2,56 +2,62 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.InputSystem;
 
 [RequireComponent(typeof(PlayerController))]
 [RequireComponent(typeof(PlayerStateManager))]
-[RequireComponent(typeof(PlayerInputReceiver))]  // Ensure PlayerInputReceiver is a required component
+[RequireComponent(typeof(PlayerInputReceiver))]
 public class PlayerAttacker : MonoBehaviour
 {
     public PlayerStateManager PlayerState;
     public PlayerController Player;
-    private PlayerInputReceiver inputReceiver;  // Reference to the PlayerInputReceiver
+    private PlayerInputReceiver inputReceiver;
+    private MoveSet moveSet;
 
     public GameObject SquareHitbox;
     public GameObject CapsuleHitbox;
 
     void Start()
     {
-        PlayerState = gameObject.GetComponent<PlayerStateManager>();
-        Player = gameObject.GetComponent<PlayerController>();
-        inputReceiver = gameObject.GetComponent<PlayerInputReceiver>();
+        PlayerState = GetComponent<PlayerStateManager>();
+        Player = GetComponent<PlayerController>();
+        inputReceiver = GetComponent<PlayerInputReceiver>();
+        moveSet = new MoveSet();
 
         // Subscribe to input events
-        inputReceiver.OnLightAttackEvent += (triggered) => StartAttack(triggered, 0.1f, 0.5f, 0.2f, SquareHitbox, new Vector3(1, 1, 1), Vector3.zero);
-        inputReceiver.OnHeavyAttackEvent += (triggered) => StartAttack(triggered, 0.2f, 0.7f, 0.3f, SquareHitbox, new Vector3(1, 1, 1), new Vector3(0, 0, 45));
-        inputReceiver.OnTrademarkAttackEvent += (triggered) => StartAttack(triggered, 0.3f, 1.0f, 0.4f, CapsuleHitbox, new Vector3(2, 2, 2), new Vector3(0, 0, 90));
+        inputReceiver.OnLightAttackEvent += (triggered) => TriggerMove(triggered, "GroundedLeftRightNormal");
+        inputReceiver.OnHeavyAttackEvent += (triggered) => TriggerMove(triggered, "GroundedLeftRightHeavy");
+        inputReceiver.OnTrademarkAttackEvent += (triggered) => TriggerMove(triggered, "LeftRightTrademark");
     }
 
     void OnDestroy()
     {
         // Unsubscribe to avoid memory leaks
-        inputReceiver.OnLightAttackEvent -= (triggered) => StartAttack(triggered, 0.1f, 0.5f, 0.2f, SquareHitbox, new Vector3(1, 1, 1), Vector3.zero);
-        inputReceiver.OnHeavyAttackEvent -= (triggered) => StartAttack(triggered, 0.2f, 0.7f, 0.3f, SquareHitbox, new Vector3(1, 1, 1), new Vector3(0, 0, 45));
-        inputReceiver.OnTrademarkAttackEvent -= (triggered) => StartAttack(triggered, 0.3f, 1.0f, 0.4f, CapsuleHitbox, new Vector3(2, 2, 2), new Vector3(0, 0, 90));
+        inputReceiver.OnLightAttackEvent -= (triggered) => TriggerMove(triggered, "GroundedLeftRightNormal");
+        inputReceiver.OnHeavyAttackEvent -= (triggered) => TriggerMove(triggered, "GroundedLeftRightHeavy");
+        inputReceiver.OnTrademarkAttackEvent -= (triggered) => TriggerMove(triggered, "LeftRightTrademark");
     }
 
-    void StartAttack(bool triggered, float startupTime, float activeTime, float endLag, GameObject hitboxPrefab, Vector3 hitboxSize, Vector3 rotation)
+    void TriggerMove(bool triggered, string moveName)
     {
-        if (triggered && PlayerState.State == PlayerStateManager.PossibleStates.FreeAction)
+        if (triggered && moveSet.moves.ContainsKey(moveName) && PlayerState.State == PlayerStateManager.PossibleStates.FreeAction)
         {
-            Debug.Log("Starting attack");
-            StartCoroutine(PerformAttack(startupTime, activeTime, endLag, hitboxPrefab, hitboxSize, rotation));
+            MoveData move = moveSet.moves[moveName];
+            StartCoroutine(PerformAttack(move.startup, move.active, move.endLag, DetermineHitbox(move.hitboxType), move.coordinates, move.rotation));
         }
     }
 
-    IEnumerator PerformAttack(float startupTime, float activeTime, float endLag, GameObject prefab, Vector3 size, Vector3 eulerAngles)
+    GameObject DetermineHitbox(string type)
+    {
+        return (type == "Square") ? SquareHitbox : CapsuleHitbox;
+    }
+
+    IEnumerator PerformAttack(float startupTime, float activeTime, float endLag, GameObject hitboxPrefab, Vector3 hitboxSize, Vector3 rotation)
     {
         PlayerState.State = PlayerStateManager.PossibleStates.PreparingAttack;
         yield return new WaitForSeconds(startupTime);
 
         PlayerState.State = PlayerStateManager.PossibleStates.Attacking;
-        GameObject activeHitbox = CreateHitbox(prefab, new Vector3(1f, 0, 0), eulerAngles, size);
+        GameObject activeHitbox = CreateHitbox(hitboxPrefab, hitboxSize, rotation);
         yield return new WaitForSeconds(activeTime);
 
         Destroy(activeHitbox);
@@ -61,10 +67,10 @@ public class PlayerAttacker : MonoBehaviour
         PlayerState.State = PlayerStateManager.PossibleStates.FreeAction;
     }
 
-    GameObject CreateHitbox(GameObject prefab, Vector3 position, Vector3 eulerAngles, Vector3 size)
+    GameObject CreateHitbox(GameObject prefab, Vector3 size, Vector3 eulerAngles)
     {
         int playerDir = Player.IsFacingRight ? 1 : -1;
-        position = new Vector3(gameObject.transform.position.x + position.x * playerDir, gameObject.transform.position.y + position.y, gameObject.transform.position.z + position.z);
+        Vector3 position = new Vector3(transform.position.x + size.x * playerDir, transform.position.y + size.y, transform.position.z + size.z);
         GameObject instance = Instantiate(prefab, position, Quaternion.Euler(eulerAngles));
         instance.transform.localScale = size;
         instance.tag = gameObject.tag;
