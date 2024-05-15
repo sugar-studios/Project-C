ff98c0e8a931fb885dcbcac71e7e2db54b8035ec
sugar studios <abcalkins8723@gmail.com>
2024-05-15 16:59:03 -0700

    fixed player attacking!!!!

--- Project-C/Assets/Scripts/Player/PlayerAttacker.cs
+++ Project-C/Assets/Scripts/Player/PlayerAttacker.cs
@@ -64,7 +64,7 @@ public class PlayerAttacker : MonoBehaviour
 
     private string GetAttackLabel(string attackType)
     {
-        string stateLabel = Player.IsGrounded() ? "ground" : "airial";
+        string stateLabel = Player.IsGrounded() ? "ground" : "aerial";
         string direction = "neutral";
 
         Vector2 inputVector = Player.PlayerInputVector;
@@ -78,7 +78,6 @@ public class PlayerAttacker : MonoBehaviour
             stateLabel = "";
         }
 
-
         Debug.Log($"{direction} {stateLabel} {attackType} attack");
         return $"{direction} {stateLabel} {attackType} attack";
     }
@@ -89,7 +88,23 @@ public class PlayerAttacker : MonoBehaviour
         Debug.Log(selectedAttack.name);
         Debug.Log(moveLabel);
         if (selectedAttack != null && PlayerState.State == PlayerStateManager.PossibleStates.FreeAction)
-            StartCoroutine(PerformAttack(selectedAttack));
+        {
+            if (selectedAttack.attackType == "projectile")
+            {
+                if (selectedAttack.chargeable)
+                {
+                    StartCoroutine(PerformChargeableProjectileAttack(selectedAttack));
+                }
+                else
+                {
+                    StartCoroutine(PerformProjectileAttack(selectedAttack));
+                }
+            }
+            else
+            {
+                StartCoroutine(PerformMultiHitAttack(selectedAttack.hits, selectedAttack.hitboxType));
+            }
+        }
     }
 
     private Attack FindAttackInMoveset(List<Attack> attacks, string moveLabel)
@@ -97,16 +112,16 @@ public class PlayerAttacker : MonoBehaviour
         return attacks.Find(attack => attack.moveLabel.Equals(moveLabel, System.StringComparison.OrdinalIgnoreCase));
     }
 
-    IEnumerator PerformAttack(Attack attack)
+    IEnumerator PerformMultiHitAttack(List<Hit> hits, string hitboxType)
     {
-        PlayerState.State = PlayerStateManager.PossibleStates.PreparingAttack;
-        yield return new WaitForSeconds(attack.hits[0].startup);
-
-        PlayerState.State = PlayerStateManager.PossibleStates.Attacking;
-        foreach (var hit in attack.hits)
+        foreach (var hit in hits)
         {
+            PlayerState.State = PlayerStateManager.PossibleStates.PreparingAttack;
+            yield return new WaitForSeconds(hit.startup);
+
+            PlayerState.State = PlayerStateManager.PossibleStates.Attacking;
             GameObject activeHitbox = CreateHitbox(
-                attack.hitboxType == "square" ? SquareHitbox : CapsuleHitbox,
+                hitboxType == "square" ? SquareHitbox : CapsuleHitbox,
                 hit.position,
                 hit.rotation,
                 hit.size * hit.scale
@@ -114,7 +129,60 @@ public class PlayerAttacker : MonoBehaviour
             yield return new WaitForSeconds(hit.active);
 
             Destroy(activeHitbox);
+            PlayerState.State = PlayerStateManager.PossibleStates.Recovering;
+            yield return new WaitForSeconds(hit.endlag);
         }
+
+        PlayerState.State = PlayerStateManager.PossibleStates.FreeAction;
+    }
+
+    IEnumerator PerformProjectileAttack(Attack attack)
+    {
+        PlayerState.State = PlayerStateManager.PossibleStates.FreeAction;
+        yield return new WaitForSeconds(attack.hits[0].startup);
+
+        PlayerState.State = PlayerStateManager.PossibleStates.Attacking;
+        GameObject projectile = CreateProjectile(
+            attack.hitboxType == "square" ? SquareHitbox : CapsuleHitbox,
+            attack.hits[0].position,
+            attack.hits[0].rotation,
+            attack.hits[0].size * attack.hits[0].scale,
+            attack.hits[0].speed,
+            attack.hits[0].duration,
+            attack.hits[0].dropOffRate,
+            attack.projectileDir
+        );
+
+        PlayerState.State = PlayerStateManager.PossibleStates.Recovering;
+        yield return new WaitForSeconds(attack.hits[0].endlag);
+
+        PlayerState.State = PlayerStateManager.PossibleStates.FreeAction;
+    }
+
+    IEnumerator PerformChargeableProjectileAttack(Attack attack)
+    {
+        PlayerState.State = PlayerStateManager.PossibleStates.PreparingAttack;
+        float chargeTime = 0f;
+        while (inputReceiver.IsCharging)
+        {
+            chargeTime += Time.deltaTime;
+            yield return null;
+        }
+
+        float maxChargeTime = attack.hits[0].maxChargeTime;
+        chargeTime = Mathf.Clamp(chargeTime, 0, maxChargeTime);
+
+        PlayerState.State = PlayerStateManager.PossibleStates.Attacking;
+        GameObject projectile = CreateProjectile(
+            attack.hitboxType == "square" ? SquareHitbox : CapsuleHitbox,
+            attack.hits[0].position,
+            attack.hits[0].rotation,
+            attack.hits[0].size * attack.hits[0].scale * (1 + chargeTime / maxChargeTime),
+            attack.hits[0].speed,
+            attack.hits[0].duration,
+            attack.hits[0].dropOffRate,
+            attack.projectileDir
+        );
 
         PlayerState.State = PlayerStateManager.PossibleStates.Recovering;
         yield return new WaitForSeconds(attack.hits[0].endlag);
@@ -130,6 +198,20 @@ public class PlayerAttacker : MonoBehaviour
         instance.transform.localScale = size;
         instance.tag = gameObject.tag;
         instance.transform.parent = this.transform;
+        return instance;
+    }
+
+    GameObject CreateProjectile(GameObject prefab, Vector3 position, Vector3 eulerAngles, Vector3 size, float speed, float duration, float dropOffRate, float projectileDir)
+    {
+        int playerDir = Player.IsFacingRight ? 1 : -1;
+        position = new Vector3(gameObject.transform.position.x + position.x * playerDir, gameObject.transform.position.y + position.y, gameObject.transform.position.z + position.z);
+        GameObject instance = Instantiate(prefab, position, Quaternion.Euler(eulerAngles)); // Keep the hitbox rotation independent
+        instance.transform.localScale = size;
+        instance.tag = gameObject.tag;
+
+        Projectile projectileScript = instance.AddComponent<Projectile>();
+        projectileScript.Initialize(speed, duration, playerDir, dropOffRate, projectileDir);
+
         return instance;
     }
 }
